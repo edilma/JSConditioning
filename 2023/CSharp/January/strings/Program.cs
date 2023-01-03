884f250044f19420ec2fac80a5ecbc5b14829854
Edilma Riano <edilmaria@hotmail.com>
2023-01-03 06:37:00 -0500

    removing vowels of a string

---
+++ 2023/CSharp/January/strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "This website is for losers LOL!";
+            Console.WriteLine(Disemvowel2(word));
+            Console.ReadLine();
+        }
+        public static string Disemvowel(string str)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            string result = "";
+            foreach (char letter in str)
+            {
+                if (!vowels.Contains(Char.ToLower(letter)))
+                {
+                    result += letter;
+                }
+            }
+            return result;
+        }
+        //BEST PRACTICE
+        public static string Disemvowel2(string str)
+        {
+            //return Regex.Replace(str, "[aeiou]", "", RegexOptions.IgnoreCase);
+            return string.Concat(str.Where(ch => !"aeiouAEIOU".Contains(ch)));
+
+        }
+    }
+}
