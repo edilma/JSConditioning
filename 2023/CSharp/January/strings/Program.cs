bfa3ada9cb67fd38172ad2055c73437050309562
Edilma Riano <edilmaria@hotmail.com>
2023-01-04 14:52:51 -0500

    formatting phone numbers

--- 2023/CSharp/January/strings/Program.cs
+++ 2023/CSharp/January/strings/Program.cs
@@ -11,10 +11,52 @@ namespace strings
     {
         static void Main(string[] args)
         {
-            string word = "This website is for losers LOL!";
-            Console.WriteLine(Disemvowel2(word));
+            //string word = "This website is for losers LOL!";
+            //Console.WriteLine(Disemvowel2(word));
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            //Console.WriteLine(CreatePhoneNumber(numbers));
+
+            Console.WriteLine(evaporator(10, 10, 5));
+
             Console.ReadLine();
         }
+        //best practice evaporator 
+        public static int evaporator2(double content, double evap_per_day, double threshold)
+        {
+            int days = 0;
+            double amountLeft = 100;
+            double evapToday = evap_per_day * amountLeft/100;
+            do
+            {
+                days++;
+                amountLeft = amountLeft - evapToday;
+                evapToday = evap_per_day * amountLeft / 100;
+            } while (amountLeft > threshold);
+            return days;
+           
+        }
+
+        public static int evaporator(double content, double evap_per_day, double threshold)
+        {
+            return (int)Math.Ceiling(Math.Log(threshold / 100.0) / Math.Log(1.0 - evap_per_day / 100.0));
+        }
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            List<string> strNum = new List<string>();
+            foreach (int item in numbers)
+            {
+                strNum.Add(item.ToString());
+
+            }
+            return $"({strNum[0]}{strNum[1]}{strNum[2]}) {strNum[3]}{strNum[4]}{strNum[5]}-{strNum[6]}{strNum[7]}{strNum[8]}{strNum[9]}";
+        }
+        //Best practice Create phone number
+        public static string CreatePhoneNumber2(int[] numbers)
+        {
+            return string.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", numbers.Select(x => x.ToString()).ToArray());
+        }
+
         public static string Disemvowel(string str)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
