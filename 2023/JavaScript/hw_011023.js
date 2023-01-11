645609a65bf4f2c7b1e8d57a4ea4f617d9b154e8
Edilma Riano <edilmaria@hotmail.com>
2023-01-10 19:22:34 -0500

    Homework of January 10, 2023

---
+++ 2023/JavaScript/hw_011023.js
@@ -0,0 +1,21 @@
+//Function that adds two numbers
+
+function addTwoNumbers(a,b) {
+    return a + b;
+}
+
+// function that returns the square of a number
+
+function square(a) {
+    return a**2;
+}
+ // object student
+
+ const student ={
+    studentId : 2564,
+    studentMajor: "Computer Science",
+    studentState: "Active"
+ }
+
+ console.log(addTwoNumbers(3,5));
+ console.log(square(6));
